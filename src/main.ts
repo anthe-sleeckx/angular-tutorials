0a52575385484d1bc93bb4b3b5f8ff9999474fd3
Anthe Sleeckx <anthe.sleeckx@gmail.com>
2025-11-27 21:21:42 +0100

    conditionals

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 //import { App } from './app/01Components/component';
-import { App } from './app/02ComponentComposition/component';
+//import { App } from './app/02ComponentComposition/component'
+import { App } from './app/03Conditionals/component';
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
