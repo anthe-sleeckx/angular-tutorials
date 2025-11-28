5121614ccda8d78181629d7132c29e62e2a44752
Anthe Sleeckx <anthe.sleeckx@gmail.com>
2025-11-28 18:52:52 +0100

    loops

---
+++ src/app/04Loops/component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: `<app-root>`,
+    template: `
+        @for (user of users; track user.id) {
+            <p>{{user.name}}</p>
+        }
+    `
+})
+
+
+export class App {
+    users = [
+        { id: 0, name: 'Anthe' },
+        { id: 1, name: 'Jef' },
+        { id: 2, name: 'Sofie' },
+        { id: 3, name: 'Thomas' },
+        { id: 4, name: 'Tim' },
+        { id: 5, name: 'PJ' }
+    ];
+}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 //import { App } from './app/01Components/component';
 //import { App } from './app/02ComponentComposition/component'
-import { App } from './app/03Conditionals/component';
-
+//import { App } from './app/03Conditionals/component';
+import { App } from './app/04Loops/component';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
