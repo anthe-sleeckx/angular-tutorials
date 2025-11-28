b10fb668561e6f9c1d8c6d21668e2fecb538a477
Anthe Sleeckx <anthe.sleeckx@gmail.com>
2025-11-28 19:02:06 +0100

    property binding

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+
+//#region App imports
 //import { App } from './app/01Components/component';
 //import { App } from './app/02ComponentComposition/component'
 //import { App } from './app/03Conditionals/component';
-import { App } from './app/04Loops/component';
+//import { App } from './app/04Loops/component';
+//#endregion
+
+import { App } from './app/05PropBinding/component';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
