f2b0b96f4c910c162135eb2760aa786bbc624233
Anthe Sleeckx <anthe.sleeckx@gmail.com>
2025-12-04 22:44:25 +0100

    ngoptimizedimage

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ import { appConfig } from './app/app.config';
 //import { App } from './app/06EventHandling/component';
 //import { App } from './app/07InputProp/component';
 //import { App } from './app/08OutputProp/component';
-import { App } from './app/09Defer/app';
+//import { App } from './app/09Defer/app';
+import { App } from './app/10OptimizedImages/user'
 //#endregion
 
 bootstrapApplication(App, appConfig)
