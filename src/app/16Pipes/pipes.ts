c0a16e81ac1b7dc4e3fe97974d050f6d103c7ff7
Anthe Sleeckx <anthe.sleeckx@gmail.com>
2025-12-15 22:52:46 +0100

    formatting with pipes

--- src/app/16Pipes/pipes.ts
+++ src/app/16Pipes/pipes.ts
@@ -4,10 +4,18 @@ import { LowerCasePipe } from '@angular/common';
 @Component({
     selector: 'app-root',
     template: `
-    {{ username | lowercase }}
+     <ul>
+      <li>Number with "decimal" {{ num }}</li>
+      <li>Date with "date" {{ birthday }}</li>
+      <li>{{ username | lowercase }}</li>
+      <li>Currency with "currency" {{ cost }}</li>
+    </ul>
   `,
     imports: [LowerCasePipe],
 })
 export class Pipes {
     username = 'anThe';
+    num = 103.1234;
+    birthday = new Date(2023, 3, 2);
+    cost = 4560.34;
 }
